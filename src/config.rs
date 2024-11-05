9fba628d96d81b5f85b096b5ebeebdced36b92ea
Maksym <bienkowski.maksym@gmail.com>
2024-11-05 20:46:07 +0100

    add keyshortcut and keymap skeleton to config file

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,36 @@
-use crossterm::event::{KeyCode, KeyModifiers};
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 
 use crate::operations::Operation;
 use std::collections::HashMap;
 
+/// Represents a single shortcut consisting of a key and modifiers
 #[derive(Debug)]
-struct Keymap {
+struct KeyShortcut {
     code: KeyCode,
     modifiers: KeyModifiers,
 }
 
+impl KeyShortcut {
+    /// Creates a new KeyShortcut from a crossterm::event::KeyEvent
+    fn from_key_event(event: &KeyEvent) -> KeyShortcut {
+        todo!()
+    }
+
+    /// Creates a new KeyShortcut based on a string from a config file
+    /// TODO: decide which syntax it has to follow and document it here
+    fn from_string(s: &str) -> KeyShortcut {
+        todo!()
+    }
+}
+
+/// Editor configuration
 #[derive(Debug)]
 pub struct Config {
-    key_mappings: HashMap<Keymap, Operation>,
+    key_mappings: HashMap<KeyShortcut, Operation>,
+}
+
+/// Loads the configuration from the config file and returns it
+fn load_config() -> Config {
+    // TODO: convert to hashmap using toml and parse keybinds
+    todo!()
 }
