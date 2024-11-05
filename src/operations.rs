76a0d0c1468d42b6af14984d2cb476b6d352bc4a
Maksym <bienkowski.maksym@gmail.com>
2024-11-05 20:46:41 +0100

    add operations

---
+++ src/operations.rs
@@ -0,0 +1,44 @@
+use std::{collections::HashMap, error::Error};
+
+// Every keymappable operation within pike
+#[derive(Debug)]
+pub enum Operation {
+    CreateNewFile,
+
+    CreateNewBuffer,
+    SwitchToPreviousBuffer,
+    SwitchToNextBuffer,
+    OpenBufferPicker,
+
+    SearchInCurrentBuffer,
+    SearchAndReplaceInCurrentBuffer,
+
+    SaveBufferToFile,
+
+    Undo,
+    Redo,
+
+    FindFilesInCWD,
+    FindTextInCWD,
+}
+
+impl Operation {
+    /// Creates a new Operation from a string from a config file
+    pub fn from_string(query: &str) -> Option<Operation> {
+        match query {
+            "new_file" => Some(Operation::CreateNewFile),
+            "new_buffer" => Some(Operation::CreateNewBuffer),
+            "previous_buffer" => Some(Operation::SwitchToPreviousBuffer),
+            "next_buffer" => Some(Operation::SwitchToNextBuffer),
+            "open_buffer_picker" => Some(Operation::OpenBufferPicker),
+            "search_in_current_buffer" => Some(Operation::SearchInCurrentBuffer),
+            "replace_in_current_buffer" => Some(Operation::SearchAndReplaceInCurrentBuffer),
+            "save" => Some(Operation::SaveBufferToFile),
+            "undo" => Some(Operation::Undo),
+            "redo" => Some(Operation::Redo),
+            "find_files_in_cwd" => Some(Operation::FindFilesInCWD),
+            "find_text_in_cwd" => Some(Operation::FindTextInCWD),
+            _ => None,
+        }
+    }
+}
