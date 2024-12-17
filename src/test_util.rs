528e4f7f3a44a5bd75d197bf2d2465a672febada
Maksym <bienkowski.maksym@gmail.com>
2024-12-17 11:16:06 +0100

    fix clippy warning

--- src/test_util.rs
+++ src/test_util.rs
@@ -2,6 +2,7 @@ use std::io::Write;
 
 use tempfile::NamedTempFile;
 
+#[allow(dead_code)] // TODO: why does clippy not see it being used in private modules?
 /// Create a temporary file with the given contents
 pub fn temp_file_with_contents(contents: &str) -> NamedTempFile {
     let mut file = NamedTempFile::new().expect("Failed to create temp file");
