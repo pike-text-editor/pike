3ddcc61e22a2c2f306c4a2b033bbb12cc12e48d6
Maksym <bienkowski.maksym@gmail.com>
2024-12-17 11:22:17 +0100

    fix clippy warning

--- src/test_util.rs
+++ src/test_util.rs
@@ -1,11 +1,15 @@
-use std::io::Write;
+#[cfg(test)]
+pub use crate::test_util::fs::*;
 
-use tempfile::NamedTempFile;
-
-/// Create a temporary file with the given contents
-pub fn temp_file_with_contents(contents: &str) -> NamedTempFile {
-    let mut file = NamedTempFile::new().expect("Failed to create temp file");
-    file.write_all(contents.as_bytes())
-        .expect("Failed to write to temp file");
-    file
+#[cfg(test)]
+pub mod fs {
+    use std::io::Write;
+    use tempfile::NamedTempFile;
+    /// Create a temporary file with the given contents
+    pub fn temp_file_with_contents(contents: &str) -> NamedTempFile {
+        let mut file = NamedTempFile::new().expect("Failed to create temp file");
+        file.write_all(contents.as_bytes())
+            .expect("Failed to write to temp file");
+        file
+    }
 }
